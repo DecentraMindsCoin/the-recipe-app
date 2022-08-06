4ab01d3f7f0afe5ec36b3c46af91249e29099c40
DecentraMindsCoin <decentraminds@gmail.com>
2022-08-05 22:57:06 -0700

    add api url

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import { useState } from "react";
 
-
+ 
 export default function Home() {
+  
+  const url = ' https://api.edamam.com/api/recipes/v2';
+
   const [recipes, setRecipes] = useState();
   const [query, setQuery] = useState();
-  
+
   return (
     <div>
       <p>The recipe app</p>
