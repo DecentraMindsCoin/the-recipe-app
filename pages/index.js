32de33485e4997f27afddbaf72f51835bb9169ee
DecentraMindsCoin <decentraminds@gmail.com>
2022-08-07 23:39:53 -0700

    add api url

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
 import { useState } from "react";
-
+import Axios from "axios"
  
 export default function Home() {
   
-  const url = ' https://api.edamam.com/api/recipes/v2';
+
 
   const [recipes, setRecipes] = useState();
   const [query, setQuery] = useState();
 
+  const url = `https://api.edamam.com/search?q=chicken&=app_id=${process.env.YOUR_APP_ID}&app_key=${process.env.YOUR_APP_KEY}`;
+  async function getRecipes() {
+    var result = await Axios.get(url);
+    console.log(result.data)
+  }
+  
   return (
     <div>
-      <p>The recipe app</p>
+      <h1 onClick={getRecipes}>The recipe app</h1>
       <input type="text" />
     </div>
   );
