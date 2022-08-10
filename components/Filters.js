e5ccd3f5c031c6d23a47601d2433cb471ae46381
DecentraMindsCoin <decentraminds@gmail.com>
2022-08-09 21:35:30 -0700

    add filters component

---
+++ components/Filters.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import React from "react";
+import { allFilters } from "../lib/recipeData";
+const Filters = () => {
+    return (
+  <div>
+
+  </div>
+)};
+
+export default Filters;
