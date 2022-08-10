ea92c0363beeb0397c14d123be3c92ac13ed7b7e
DecentraMindsCoin <decentraminds@gmail.com>
2022-08-09 21:45:27 -0700

    change componets dom structure

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,96 @@
-import { useState } from "react";
-import Axios from "axios"
-import Hotkeys from "../components/hotkeys";
+import { useEffect, useState } from "react";
+import Axios from "axios";
+import Recipes from "../components/Recipes";
+import { recipeList, allFilters } from "../lib/recipeData";
+import Image from "next/image";
+import Header from "../components/Header";
+import SearchArea from "../components/SearchArea";
+import Filters from "../components/Filters";
 
 export default function Home() {
-  
+  const [recipes, setRecipes] = useState(recipeList);
+  const [query, setQuery] = useState("");
+  const [filters, setFilter] = useState(allFilters);
+  const [comments, setComments] = useState()
+    const handleQuery = () => {
+    useEffect((e) => {
+      e.preventDefault();
+      setRecipes(recipes);
+      setQuery(query);
+    });
+  };
 
-
-  const [recipes, setRecipes] = useState();
-  const [query, setQuery] = useState();
-
-  const url = `https://api.edamam.com/search?q=chicken&=app_id=${process.env.YOUR_APP_ID}&app_key=${process.env.YOUR_APP_KEY}`;
-  async function getRecipes() {
-    var result = await Axios.get(url);
-    console.log(result.data)
-  }
-  
+  console.log(recipes);
+  console.log(query);
   return (
     <div>
-      <h1 className="text-pink-500 font-bold" onClick={getRecipes}>The recipe app</h1>
-      <input type="text" className="border-2  rounded-full cursor pointer" />
-      <Hotkeys />
+      <Header />
+      <div className="align-items-center justify-center flex-row max-w-2xl mx-auto ">
+        <form onSubmit={handleQuery} className="flex">
+          <input
+            value={query}
+            type="text"
+            placeholder="Search"
+            onChange={(e) => setQuery(e.target.value)}
+            className="py-2 px-4 hover:outline-green-500 hover:shadow-xl hover:shadow-green-500  outline-none border-2 w-full flex rounded-full cursor pointer my-10"
+          />
+
+          <button type="submit" value="Search">
+            <p
+              className=" bg-black text-white font-bold px-4 py-2 rounded-tr-xl"
+              onClick={handleQuery}
+            >
+              Button
+            </p>
+          </button>
+        </form>
+        <div className="w-full h-full flex overflow-x-scroll ">
+          {filters.map((item) => {
+            return (
+              <div
+                key={item.id}
+                className="w-40 h-40 mx-1 object-cover relative border-4 border-green-500 rounded-xl"
+              >
+             <Image
+                  src={item.url}
+                  alt=""
+                  layout="fill"
+                  className="px-5 absolute flex object-cover"
+                /> 
+                
+              </div>
+            );
+          })}
+        </div>
+        {recipes.map((r) => {
+          return (
+            <div key={r.id} className="py-20 mx-4 space-y-6">
+              <h2 className="font-bold text-3xl">{r.name}</h2>
+              <p className="text-lg">{r.description}</p>
+              <div className="relative h-full w-full flex border-8 rounded-xl border-green-500">
+                <Image
+                  width={1000}
+                  height={700}
+                  src={r.image}
+                  alt=""
+                  className="object-cover absolute"
+                />
+              </div>
+              <div className="w-full flex justify-between items-center font-bold">
+                <div className="flex space-x-3">
+                  <p>{r.likes}</p>
+                  <p>{r.adds}</p>
+                </div>
+                <div className="bg-green-500 px-5 py-2">+</div>
+              </div>
+              <div>
+                <button></button>
+                {!comments && <div><p>comments</p><textarea value={comments} placeholder="leave comment" className="w-full h-20 border-gray-300 border-2 px-3 py-2 "  /></div>}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
