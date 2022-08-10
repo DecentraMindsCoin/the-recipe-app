4b1eeabddb1c7541a46f6446bf15892fcdcd837f
DecentraMindsCoin <decentraminds@gmail.com>
2022-08-09 21:36:02 -0700

    add search area component

---
+++ components/SearchArea.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SearchArea = ({ recipes, setRecipes, setQuery, query, onSubmit }) => {
+
+  return (
+    <div>
+
+
+    </div>
+  );
+};
+
+export default SearchArea;
