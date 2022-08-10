2f972f32a3444b2d999772eddba3b199f2d18f6c
DecentraMindsCoin <decentraminds@gmail.com>
2022-08-09 21:36:43 -0700

    add Recipes component

---
+++ components/Recipes.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import React, { useState } from "react";
+
+const Recipes = ({ rec }) => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Recipes;
